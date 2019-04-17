cd6ab9aebcf7cc6c1e6c76c6a9655f1c936e61a5
Howaajin <howaajin@outlook.com>
2019-04-17 14:49:40 +0800

    Added missing copyright information in FastAndSimplePositioningStrategy.cpp

--- Source/GraphFormatter/Private/FastAndSimplePositioningStrategy.cpp
+++ Source/GraphFormatter/Private/FastAndSimplePositioningStrategy.cpp
@@ -1,4 +1,9 @@
-﻿#include "FastAndSimplePositioningStrategy.h"
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Howaajin. All rights reserved.
+ *  Licensed under the MIT License. See License in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+#include "FastAndSimplePositioningStrategy.h"
 #include "FormatterGraph.h"
 #include "FormatterSettings.h"
 
