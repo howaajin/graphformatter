de0db96366f32768acc95ea5d9727e9c14f83468
howaajin <howaajin@outlook.com>
2022-12-28 19:39:17 +0800

    Added 'LogGraphFormatter' log category

---
+++ Source/GraphFormatter/Private/FormatterLog.cpp
@@ -0,0 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Howaajin. All rights reserved.
+ *  Licensed under the MIT License. See License in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+#include "FormatterLog.h"
+
+DEFINE_LOG_CATEGORY(LogGraphFormatter);
---
+++ Source/GraphFormatter/Private/FormatterLog.h
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Howaajin. All rights reserved.
+ *  Licensed under the MIT License. See License in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogGraphFormatter, VeryVerbose, All);
+
