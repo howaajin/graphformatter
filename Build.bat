23d83e2a78ef6eb364d1968669b6308815c62781
howaajin <howaajin@outlook.com>
2023-01-02 22:35:53 +0800

    Make the archived file contain a folder prefix

--- Build.bat
+++ Build.bat
@@ -18,7 +18,7 @@ FOR /F "usebackq skip=2 tokens=2*" %%A IN (`REG QUERY %KEY_NAME% /v %VALUE_NAME%
 if defined UE_PATH (
     @echo Unreal engine %~1 = %UE_PATH%
     @echo Building plugin for Unreal engine %~1...
-    call "%UE_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=%DES_FILE% -Package="%BUILD_DIR%\%~1"
+    call "%UE_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=%DES_FILE% -Package="%BUILD_DIR%\%~1\GraphFormatter"
     if not ERRORLEVEL 0 (
         exit /b 1
     )
@@ -34,7 +34,10 @@ if defined UE_PATH (
         @echo skip packaging...
         exit /b 0
     )
-    7z a "%BUILD_DIR%\GraphFormatter%~1.zip" "%BUILD_DIR%\%~1\Resources" "%BUILD_DIR%\%~1\Source" "%BUILD_DIR%\%~1\GraphFormatter.uplugin"
+    pushd .
+    cd /D "%BUILD_DIR%\%~1\"
+    7z a "%BUILD_DIR%\GraphFormatter%~1.zip" GraphFormatter\Resources\ GraphFormatter\Source\ GraphFormatter\GraphFormatter.uplugin
+    popd
 ) else (
     @echo Unreal engine %~1 not found. Skip build for %~1...
     exit /b 0
