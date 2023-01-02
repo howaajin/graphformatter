8590d86e53f2dc336d784d01451fe4a779296984
howaajin <howaajin@outlook.com>
2023-01-02 19:17:03 +0800

    Added -install -enabled to Build.bat.
    with parameter -install will copy the plugin to Marketplace folder.
    with parameter -enabled will make the plugin enabled for all project.

--- Build.bat
+++ Build.bat
@@ -7,8 +7,8 @@ set BUILD_DIR=%~dp0\Build
 
 if not exist %BUILD_DIR% mkdir %BUILD_DIR% 
 
-call :Build 5.0
-call :Build 5.1
+call :Build 5.0 %~1 %~2
+call :Build 5.1 %~1 %~2
 goto RET
 
 :Build
@@ -19,17 +19,26 @@ if defined UE_PATH (
     @echo Unreal engine %~1 = %UE_PATH%
     @echo Building plugin for Unreal engine %~1...
     call "%UE_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildPlugin -Plugin=%DES_FILE% -Package="%BUILD_DIR%\%~1"
+    if not ERRORLEVEL 0 (
+        exit /b 1
+    )
+    if "%~2" == "-install" (
+        xcopy "%BUILD_DIR%\%~1\*" "%UE_PATH%\Engine\Plugins\Marketplace\GraphFormatter\" /s /e /y
+    )
+    if "%~3" == "-enabled" (
+        xcopy "%DES_FILE%" "%UE_PATH%\Engine\Plugins\Marketplace\GraphFormatter\GraphFormatter.uplugin" /y
+    )
     where 7z.exe >nul 2>nul
     IF NOT ERRORLEVEL 0 (
         @echo 7z.exe not found.
         @echo skip packaging...
-        EXIT /B 0
+        exit /b 0
     )
     7z a "%BUILD_DIR%\GraphFormatter%~1.zip" "%BUILD_DIR%\%~1\Resources" "%BUILD_DIR%\%~1\Source" "%BUILD_DIR%\%~1\GraphFormatter.uplugin"
 ) else (
     @echo Unreal engine %~1 not found. Skip build for %~1...
-    EXIT /B 0
+    exit /b 0
 )
-EXIT /B 0
+exit /b 0
 
 :RET
